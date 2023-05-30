b9da87cb9aabaa8e85ad29f56e31941d1c23fa70
tauin <71744831+tauin@users.noreply.github.com>
2023-05-31 01:06:48 +0800

    add temp logging

--- scripts/tau/state-based-health/player.lua
+++ scripts/tau/state-based-health/player.lua
@@ -23,13 +23,21 @@ local FORTIFY_HEALTH = core.magic.EFFECT_TYPE.FortifyHealth
 local function setHealth()
    local previousBaseHealth = health.base
    local previousCurrentHealth = health.current
+   print(string.format("Old max health: %f", previousBaseHealth))
+   print(string.format("Old current health: %f", previousCurrentHealth))
 
    enduranceState = endurance.modified
    strengthState = strength.modified
    levelState = level.current
 
+   print(string.format("Endurance: %f", enduranceState))
+   print(string.format("Strength: %f", strengthState))
+   print(string.format("Level: %d", levelState))
+
    local newBaseHealth = ((enduranceState + strengthState) / 2)
       + ((levelState - 1) * F_LEVEL_UP_HEALTH_END_MULT * enduranceState)
+   print(string.format("fLevelUpHealthEndMult GMST: %f", F_LEVEL_UP_HEALTH_END_MULT))
+   print(string.format("New max health (per formula): %f", newBaseHealth))
 
    -- In Base Morrowind, potions increase current health, while spells increase maximum health
    -- MCP adds the option to have potions also increase maximum health
@@ -44,31 +52,47 @@ local function setHealth()
       newBaseHealth = newBaseHealth + fortifyHealthMag
    end
 
+   print(string.format("Fortify Health magnitude: %f", fortifyHealthMag))
+
    newBaseHealth = math.max(newBaseHealth, conf:get("minBaseHealth"))
+   print(string.format("Minimum max health setting: %f", conf:get("minBaseHealth")))
+   print(string.format("New max health (taking into account minimum max health setting): %f", newBaseHealth))
 
    local newCurrentHealth
    if conf:get("maintainAbsoluteDifference") then
       local HealthDifference = previousBaseHealth - previousCurrentHealth
       newCurrentHealth = newBaseHealth - HealthDifference
+
+      print(string.format("Maintaining difference. Difference: %f", HealthDifference))
+      print(string.format("New current health: %f", newCurrentHealth))
    else
       local HealthRatio = health.current / health.base
       newCurrentHealth = newBaseHealth * HealthRatio
+
+      print(string.format("Maintaining ratio. Ratio: %f", HealthRatio))
+      print(string.format("New current health: %f", newCurrentHealth))
    end
 
    if fortifyHealthMag > 0 and not conf:get("maintainAbsoluteDifference") then
+      print("There is a Fortify Health magnitude, and we are maintaining ratio. Adjusting new current health to compensate.")
       local previousCurrentHealthSansFortify = previousCurrentHealth - fortifyHealthMag
 
       local ratioSansFortify = previousCurrentHealthSansFortify / previousBaseHealth
 
-      local currentHealthWithFortify = newBaseHealth * ratioSansFortify
+      local currentHealthSansFortify = newBaseHealth * ratioSansFortify
 
       if previousCurrentHealthSansFortify >= 0 then
-         newCurrentHealth = currentHealthWithFortify + fortifyHealthMag
+         newCurrentHealth = currentHealthSansFortify + fortifyHealthMag
       end
+      print(string.format("Old current health would be without Fortify Health: %f", previousCurrentHealthSansFortify))
+      print(string.format("Health ratio would be without Fortify Health: %f", ratioSansFortify))
+      print(string.format("Current health needs to be when Fortify Health expires: %f", currentHealthSansFortify))
+      print(string.format("New current health, taking into account Fortify Health: %f", newCurrentHealth))
    end
 
    health.base = newBaseHealth
    health.current = newCurrentHealth
+
 end
 
 return {
